be396e6d1d64f1a4021f241d6e5641f0ab70f199
Tamas Szabo <szabo.tamas.mk@gmail.com>
2020-10-22 12:08:54 +0200

    First commit

---
+++ src/build/precache-manifest.e4abfff837bd1a456f167ee13fb6f658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a2ca8f6d6bc2d15b5928e2bfaa9d96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ad7f63b881bcfddf7c6",
+    "url": "/static/css/main.faeaa1d6.chunk.css"
+  },
+  {
+    "revision": "27ea7e2fac58a861525a",
+    "url": "/static/js/2.fa93b14c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.fa93b14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad7f63b881bcfddf7c6",
+    "url": "/static/js/main.fa4eb70f.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
